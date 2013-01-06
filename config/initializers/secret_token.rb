c1b29175db7b9b99da1ac0bc81895d1e6ab121ab
Martin Provencher <mprovencher86@gmail.com>
2013-01-06 14:40:08 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsbridgeMontreal::Application.config.secret_token = '839453aff0aea7d23e0b94677a23e15362dc45100562b71840ee7bfcf9a2221e8f365919dd24b6e40acc83de47a156e6f12afe6b7e419339462c54e90e7b2b06'
