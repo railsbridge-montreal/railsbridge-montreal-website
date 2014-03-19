66647b9145a966958e0d4ee067e5d9ad79be6364
Mathieu Gagne <gagne.mathieu@hotmail.com>
2014-03-19 09:37:48 -0400

    Adds additional information for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Railsbridge::Application.routes.draw do
 
   resources :registrants
+  # Quick fix for 404 from tweet
   get '/register', to: redirect('/registration')
 
   post "/mailer"  , to: 'messages#create' , as: :mailer
 
+  # More information on localized routes here: https://github.com/enriclluelles/route_translator
+  # Actual paths in locale files, i.e. config/locales/(en|fr).yml
   localized do
     get "/team"     , to: 'pages#team'
     get '/register' , to: 'registrants#new'
