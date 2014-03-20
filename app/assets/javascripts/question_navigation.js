5a1c77e36605497b779765eede692a6c3c4c4d80
Richard McGain <richard.mcgain@shopify.com>
2014-03-20 00:39:13 -0400

    add validation to email

--- app/assets/javascripts/question_navigation.js
+++ app/assets/javascripts/question_navigation.js
@@ -1,4 +1,4 @@
-function goForward(current_fs, next_fs){
+function goForward(current_fs, next_fs) {
   var current_fs, next_fs, previous_fs; //fieldsets
   var left, opacity, scale; //fieldset properties which we will animate
   var animating; //flag to prevent quick multi-click glitches
@@ -63,18 +63,37 @@ function answerQuestion(event) {
 }
 
 function addNameAndEmailToNewForm() {
-  var name = $('#registrant_email')[0].value
-  var email = $('#registrant_name')[0].value
-  $('#registrant_hidden_name')[0].value = name
-  $('#registrant_hidden_email')[0].value = email
+  var name = $('#registrant_name')[0].value;
+  var email = $('#registrant_email')[0].value;
+  $('#registrant_hidden_name')[0].value = name;
+  $('#registrant_hidden_email')[0].value = email;
+  console.log(name + " " + email);
+}
+
+function validate() {
+ $('.registration-form .registration-message').remove();
+  var name = $('#registrant_name')[0].value;
+  var email = $('#registrant_email')[0].value;
+
+  if (name.length < 1) {
+    $('#registrant_name').addClass('has-error');
+    $('.registration-form').append("<div class='registration-message'>Please enter your name</div>");
+  }
+  else if (!(/\S+@\S+\.\S+/.test(email))) {
+    $('#registrant_email').addClass('has-error');
+  $('.registration-form').append("<div class='registration-message'>Please enter your email</div>");
+  }
+  else {
+    addNameAndEmailToNewForm();
+    openQuestions();
+  }
 }
 
 $(document).ready(function() {
   $('.registration-form').on('submit', function() {
     event.preventDefault();
     event.stopPropagation();
-    addNameAndEmailToNewForm();
-    openQuestions();
+    validate();
   });
 
   $('.questions-form input[type="radio"]').click(function(event) {
