a4181aab75791314fa2390354cafd5eea5ac519e
flohdot <floh@flohdot.org>
2014-03-30 16:43:30 -0400

    placed scope

--- app/models/registrant.rb
+++ app/models/registrant.rb
@@ -4,6 +4,7 @@ class Registrant < ActiveRecord::Base
   validates :email, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
   scope :unplaced, -> { where(course: nil, cancelled_at: nil) }
+  scope :placed, -> { where("course is not null and cancelled_at is null") }
   scope :active, -> { where(cancelled_at: nil, waitlisted: false) }
 
 end
