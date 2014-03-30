eed89b0bf1813724123577391af58773c13445b5
flohdot <floh@flohdot.org>
2014-03-29 23:00:53 -0400

    scopes for unplaced registrants and active (as opposed to waitlisted/cancelled) registrants

--- app/models/registrant.rb
+++ app/models/registrant.rb
@@ -3,4 +3,7 @@ class Registrant < ActiveRecord::Base
   validates :name, presence: true
   validates :email, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
+  scope :unplaced, -> { where(course: nil, cancelled_at: nil) }
+  scope :active, -> { where(cancelled_at: nil, waitlisted: false) }
+
 end
