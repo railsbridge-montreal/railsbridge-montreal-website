8bfa2e8ddf88f589421c42b82336d33bdca6798e
Sophie Deziel <sdeziel@acquisio.com>
2016-09-13 00:25:11 -0400

    close the registrations automatically

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_registration
+
+  def set_registration
+    @waitlisting = Edition.find(2).registrants.active.count >= 120
+  end
 
   protected
 
